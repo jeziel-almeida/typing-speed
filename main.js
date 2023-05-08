581790f57e5f349b2e456d49ba9226da30ec4a0a
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-05-08 22:38:55 +0000

    Create import text from a json file

--- main.js
+++ main.js
@@ -14,51 +14,23 @@ let wordsCount = 0
 let historyBlanked = true
 let index = 0
 let currentLanguage = 'english'
+let texts = ['Loading...']
+let textsHogwarts = []
+let normalTextsPt = []
+let normalTextsEn = []
 
-const textsHogwarts = [
-    "When in doubt, go to the library",
-    "I solemnly swear that I am up to no good",
-    "Hogwarts will always be there to welcome you home",
-    "Don't let the muggles get you down",
-    "It does not do to dwell on dreams and forget to live",
-    "I mean, it's sort of exciting, isn't it, breaking the rules?",
-    "Wit beyond measure is man's greatest treasure",
-    "Once again, you show all the sensitivity of a blunt axe",
-    "Anyone can speak Troll. All you have to do is point and grunt",
-    "We are only as strong as we are united, as weak as we are divided"
-]
 
-const normalTextsPt = [
-    "Enquanto eu tiver perguntas e não houver resposta continuarei a escrever",
-    "Ela acreditava em anjo e, porque acreditava, eles existiam",
-    "Liberdade é pouco. O que desejo ainda não tem nome",
-    "E o que o ser humano mais aspira é tornar-se ser humano",
-    "Passei a vida tentando corrigir os erros que cometi na minha ânsia de acertar",
-    "Divertir os outros é um dos modos mais emocionantes de existir",
-    "Cada qual sabe amar a seu modo; o modo, pouco importa; o essencial é que saiba amar",
-    "O destino, como todos os dramaturgos, não anuncia as peripécias nem o desfecho",
-    "Defeitos não fazem mal, quando há vontade e poder de os corrigir",
-    "Podemos julgar o coração de um homem pela forma como ele trata os animais"
-]
-
-const normalTextsEn = [
-    "There can always be healing, even if there is no cure",
-    "What you do is more important than what you say",
-    "It's better to finish something late than to never do it at all",
-    "A journey of a thousand miles begins with a single step",
-    "Anxiety weighs down the heart, but a kind of words cheers it up",
-    "All that is gold does not glitter, not all those who wander are lost",
-    "Still round the corner there may wait a new road or a secret gate",
-    "All we have to decide is what to do with the time that is given us",
-    "There is a wisdom of the head, and a wisdom of the heart",
-    "No one is useless in this world who lightens the burden of it to anyone else"
-]
-
-let texts = normalTextsEn
+async function importTexts() {
+    const res = await fetch('texts/texts.json')
+    const json = await res.json()
+    textsHogwarts = json["textsHogwarts"]
+    normalTextsPt = json["normalTextsPt"]
+    normalTextsEn = json["normalTextsEn"]
+    texts = normalTextsEn
+    newText()
+}
 
 function newText() {
-    // const index = Math.floor(Math.random() * texts.length)
-    // textChosen = texts[index]
     if(index >= 10) {
         index = 0
     }
@@ -116,7 +88,8 @@ function verify() {
 function addToHistory(typedText, timeSpent, WPM) {
 
     if(historyBlanked) {
-        history.innerHTML = ""
+        history.classList.remove("hidden")
+        document.getElementById("history-temp").classList.add("hidden")
     }
 
     const itemHistory = document.createElement("p")
@@ -134,7 +107,10 @@ function restartTest() {
     result.textContent = "Seconds: X.X / WPM: XX"
     newText()
     localStorage.setItem("testInCourse", false)
+    removeClock()
     history.innerHTML = ""
+    history.classList.add("hidden")
+    document.getElementById("history-temp").classList.remove("hidden")
     text.classList.remove("wrong-phrase", "animate__animated", "animate__shakeX")
 }
 
@@ -142,7 +118,7 @@ input.addEventListener("keyup", updateTest)
 restart.addEventListener("click", restartTest)
 localStorage.setItem("testInCourse", false)
 
-newText()
+importTexts()
 
 function showClock() {
     document.getElementById("command-container").classList.add("hidden")
